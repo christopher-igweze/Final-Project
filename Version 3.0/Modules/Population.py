6afd69b31da600180bb949ee176dee759aa7bee7
Ifeanyi Igweze <christopher.igweze@gmail.com>
2024-05-18 22:16:26 +0100

    Added line by line comments to the population file

--- Version 3.0/Modules/Population.py
+++ Version 3.0/Modules/Population.py
@@ -1,10 +1,22 @@
+# Importing the Schedule class from the Modules package
 from Modules.Schedule import Schedule
+# Importing the dbMgr module from the Modules package
 from Modules import dbMgr
 
+# Defining the Population class
 class Population:
+    # Constructor for the Population class
     def __init__(self, size):
+        # Setting the size of the population
         self._size = size
+        # Storing a reference to the dbMgr module
         self._data = dbMgr
+        # Initializing an empty list to hold the schedules
         self._schedules = []
-        for i in range(0, size): self._schedules.append(Schedule().initialize())
-    def get_schedules(self): return self._schedules
+        # For each number in the range from 0 to size
+        for i in range(0, size):
+            # Append a new, initialized Schedule to the list of schedules
+            self._schedules.append(Schedule().initialize())
+    # Method to get the list of schedules
+    def get_schedules(self): 
+        return self._schedules
