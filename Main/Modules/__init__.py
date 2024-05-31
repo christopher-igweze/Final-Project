b8082fce5d91b1aca646168d6d96c470612f56ab
Ifeanyi Igweze <christopher.igweze@gmail.com>
2024-05-31 13:34:52 +0100

    Deleted one of the old scheduler folder.

--- Main/Modules/__init__.py
+++
@@ -1,6 +0,0 @@
-# relatively imports from Modules.Db_Manager
-from .Db_Manager import DBMgr
-import random as rnd
-POPULATION_SIZE = 1000
-
-dbMgr = DBMgr()
--- Main/Timetabler.py
+++
@@ -1,8 +0,0 @@
-from Modules.Display import handle_command_line
-VERBOSE_FLAG = False
-
-def main():
-    handle_command_line(VERBOSE_FLAG)
-
-if __name__ == "__main__":
-    main()
